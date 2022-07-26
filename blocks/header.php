d4e823f2c03076bcb378153c459d1e068d2e6364
Ильяс Илимбаев <ilyas.ilimbaev@yandex.ru>
2022-07-26 19:12:39 +0500

    Tut y nas verstka i programmirovanie chego-to, ostavil, tak kak izmeneniya nujnoe

---
+++ blocks/header.php
@@ -0,0 +1,107 @@
+<header class="header">
+    <div class="header__wrapper">
+        <div class="container">
+            <div class="header-top">
+                <div class="header-nav">
+                    <div class="header-nav-left">
+                        <a href="index.html" class="logo">
+                            <span class="logo-text">
+                                <span class="green-text">ПОЛЬЗА</span> ДОМ
+                            </span>
+                            <svg class="icon logo-icon">
+                                <use xlink:href="img/sprite.svg#logo"></use>
+                            </svg>
+                        </a>
+                        <ul class="header-list">
+                            <li class="header-list-item">
+                                <a href="catalog.html" class="header-list-link">Каталог</a>
+                            </li>
+                            <li class="header-list-item">
+                                <a href="services.html" class="header-list-link">Услуги</a>
+                            </li>
+                            <li class="header-list-item">
+                                <a href="" class="header-list-link">О компании</a>
+                            </li>
+                            <li class="header-list-item">
+                                <a href="uslugi/proektirovanie.html" class="header-list-link">Проектирование</a>
+                            </li>
+                            <li class="header-list-item">
+                                <a href="" class="header-list-link">Этапы работ</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="header-nav-right">
+                        <a href="" class="user-login-link item-icon">
+                            <svg class="icon">
+                                <use xlink:href="img/sprite.svg#user-profile"></use>
+                            </svg>
+                        </a>
+                        <div class="header-search-item">
+                            <form action="" class="header-search-form">
+                                <input type="search" name="s" maxlength="16" id="search-input" class="search-input" placeholder="Поиск по сайту">
+                                <button id="search-button" class="search-button header-top-item-search" type="submit">
+                                    <svg class="icon search-icon">
+                                        <use xlink:href="img/sprite.svg#search-icon"></use>
+                                    </svg>
+                                </button>
+                            </form>
+                        </div>
+                        <div class="toggle-menu">
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="header-menu">
+                    <ul class="header-menu-list">
+                        <li class="header-menu-item">
+                            <a href="catalog.html" class="header-menu-link">Каталог</a>
+                        </li>
+                        <li class="header-menu-item">
+                            <a href="services.html" class="header-menu-link">Услуги</a>
+                        </li>
+                        <li class="header-menu-item">
+                            <a href="" class="header-menu-link">О компании</a>
+                        </li>
+                        <li class="header-menu-item">
+                            <a href="" class="header-menu-link">Этапы строительства</a>
+                        </li>
+                        <li class="header-menu-item">
+                            <a href="uslugi/proektirovanie.html" class="header-menu-link">Проектирование</a>
+                        </li>
+                        <li class="header-menu-item">
+                            <a href="ourworks.html" class="header-menu-link">Наши работы</a>
+                        </li>
+                        <li class="header-menu-item">
+                            <a href="" class="header-menu-link">Ипотека и кредит</a>
+                        </li>
+                        <li class="header-menu-item header-menu-line"></li>
+                        <li class="header-menu-item">
+                            <a href="" class="header-menu-link">Отзывы</a>
+                        </li>
+                        <li class="header-menu-item">
+                            <a href="" class="header-menu-link">Контакты</a>
+                        </li>
+                        <li class="header-menu-item">
+                            <a href="" class="header-menu-link">Вопрос - ответ</a>
+                        </li>
+                        <li class="header-menu-item">
+                            <a href="#" class="header-menu-link link-login">Вход. Регистрация</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>        
+    <div class="header-content">
+        <!-- <div class="header-content__img">
+            <img src="img/header-home.png" alt="" srcset="">
+        </div> -->
+        <div class="header-content__desc">
+            <div class="header-text-wrapper">
+                <h1 class="header-title">строительство домов <span class="green-text">под ключ</span></h1>
+                <p class="header-text">Качественное строительство домов по доступной стоимости</p>
+                <a href="" class="header-calc-link">Рассчитать стоимость</a>
+            </div>
+        </div>
+    </div>
+</header>
